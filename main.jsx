da93ad8b216976b6b0eab848a20bdeb900e8722d
MoazAlaa7 <moazalaa4727@gmail.com>
2026-07-01 02:40:11 +0300

    create a miragejs server

--- main.jsx
+++ main.jsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import "./server";
+
 createRoot(document.getElementById("root")).render(<App />);

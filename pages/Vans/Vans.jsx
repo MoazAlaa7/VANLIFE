4816d002189df69cdb63c61bc860d7c7d689b3c7
MoazAlaa7 <moazalaa4727@gmail.com>
2026-07-09 06:15:46 +0300

    Add Vans filtering functionality

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const typeFilter = searchParams.get("type");
 
   useEffect(() => {
     fetch("/api/vans")
@@ -10,7 +13,11 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => {
+  const filteredVans = typeFilter
+    ? vans.filter((van) => van.type.toLowerCase() === typeFilter)
+    : vans;
+
+  const vanElements = filteredVans.map((van) => {
     return (
       <div key={van.id} className="van-container">
         <Link to={van.id}>
@@ -31,6 +38,34 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <button
+          className={`van-type simple ${typeFilter === "simple" ? "selected" : ""}`}
+          onClick={() => setSearchParams({ type: "simple" })}
+        >
+          Simple
+        </button>
+        <button
+          className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`}
+          onClick={() => setSearchParams({ type: "luxury" })}
+        >
+          Luxury
+        </button>
+        <button
+          className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`}
+          onClick={() => setSearchParams({ type: "rugged" })}
+        >
+          Rugged
+        </button>
+        {typeFilter ? (
+          <button
+            onClick={() => setSearchParams({})}
+            className="clear-filters-button"
+          >
+            Clear filters
+          </button>
+        ) : null}
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );

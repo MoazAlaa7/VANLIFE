e6178a4827dd37142cc4bf2226c547debe84e69d
MoazAlaa7 <moazalaa4727@gmail.com>
2026-07-10 04:25:40 +0300

    Add login page link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from "react-router";
+import imageUrl from "/assets/images/avatar-icon.png";
 
 export default function Header() {
   return (
@@ -27,6 +28,9 @@ export default function Header() {
         >
           Vans
         </NavLink>
+        <Link to="/login" className="login-link">
+          <img src={imageUrl} className="login-icon" />
+        </Link>
       </nav>
     </header>
   );
